b63799cfe842282bedcb1bc965dc50d96f0060d0
loethor <lrb2392@gmail.com>
2023-02-03 18:09:52 +0100

    Added wheat mesh

--- Wheat.gd
+++ Wheat.gd
@@ -7,11 +7,11 @@ func _ready() -> void:
 func _on_Area_body_entered(body: Node) -> void:
 	print(body)
 	if body.name == "wheat_collector_body":
-		$MeshInstance.hide()
+		$Wheat.hide()
 		$Area/CollisionShape.disabled = true
 		body.get_parent().get_parent().get_parent().get_node("Player").wheat_counter += 1
 		$WheatTimer.start()
 
 func _on_WheatTimer_timeout() -> void:
-	$MeshInstance.show()
+	$Wheat.show()
 	$Area/CollisionShape.disabled = false
